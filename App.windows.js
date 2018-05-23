df3a4332a152267cae81c1768908b6a5bd9c99c6
Brent Gray <bgray@oncoursesystems.com>
2018-05-23 17:02:43 -0400

    first commit

---
+++ App.windows.js
@@ -0,0 +1,89 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+const bookList = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  }, {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }];
+
+const instructions =
+  'Press Ctrl+R to reload,\n' +
+  'Shift+F10 or shake for dev menu';
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image 
+          style={{width: 500, height: 263}}
+          source={{uri:  'https://www.tablexi.com/wp-content/uploads/2017/12/ReactNative.png'}}
+        />
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+        <Text style={styles.instructions}>
+          {bookList.map(function(item) {
+            return (
+              <Text key={item.objectID} style={styles.titleText}>{item.title}{'\n'}</Text>
+            };
+          })}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  titleText: {
+    marginBottom: 5,
+    fontWeight: 'bold',
+    color: 'blue'
+  },
+  detailText: {
+    fontWeight: 'normal',
+    color: 'black'
+  }
+});
